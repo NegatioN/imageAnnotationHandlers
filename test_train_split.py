d368fa87fcd794f9937a7b1f517508d8aa28c47e
NegatioN <joakimrishaug@gmail.com>
2017-01-06 00:21:30 +0100

    add validation option

--- test_train_split.py
+++ test_train_split.py
@@ -9,12 +9,23 @@ def make_folder(image_type):
     train_dir = '{}/{}/{}'.format(output_path, "train", image_type)
     if not os.path.exists(train_dir):
         os.makedirs(train_dir)
+    valid_dir = '{}/{}/{}'.format(output_path, "valid", image_type)
+    if not os.path.exists(valid_dir):
+        os.makedirs(valid_dir)
 
 def copy_files(image_type, test_or_train, file_list):
     for filename in file_list:
         shutil.copyfile('{}/{}/{}'.format(input_path, image_type, filename),
                         '{}/{}/{}/{}'.format(output_path, test_or_train, image_type, filename))
-train_percent = 80
+
+def calc_index(image_list, percentage):
+    return int((float(len(image_list)) / 100)*percentage)
+
+
+train_percent = 70
+test_percent = 20
+valid_percent = 100-(train_percent+test_percent)
+
 
 input_path = "output_ordered"
 output_path = 'test_train'
@@ -29,8 +40,11 @@ print(image_names_types)
 
 for image_type, image_list in image_names_types.items():
     print(image_list)
-    train_num = int((float(len(image_list)) / 100)*train_percent)
+    train_num = calc_index(image_list, train_percent)
+    test_num = calc_index(image_list, test_percent) + train_num
     train_files = image_list[:train_num]
-    test_files = image_list[train_num:]
+    test_files = image_list[train_num:test_num]
+    valid_files = image_list[test_num:]
     copy_files(image_type, 'train', train_files)
     copy_files(image_type, 'test', test_files)
+    copy_files(image_type, 'valid', valid_files)
